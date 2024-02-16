d78c0844038156b2d95a69f302a44f49e4cce57a
Esley <esleynathan@hotmail.com>
2024-02-16 01:48:57 -0300

    Criando Endpoint de Atrações

--- core/models.py
+++ core/models.py
@@ -4,14 +4,14 @@ from comentarios.models import Comentario
 from avaliacoes.models import Avaliacao
 from enderecos.models import Endereco
 
-class  PontoTuristico(models.Model):
+class PontoTuristico(models.Model):
     nome = models.CharField(max_length=150)
     descricao = models.TextField()
     aprovado = models.BooleanField(default=False)
-    atracao = models.ManyToManyField(Atracao)
-    comentarios = models.ManyToManyField(Comentario)
-    avaliacoes = models.ManyToManyField(Avaliacao)
-    endereco = models.ForeignKey(Endereco, on_delete=models.CASCADE)
+    atracao = models.ManyToManyField(Atracao, null=True, blank=True)
+    comentarios = models.ManyToManyField(Comentario, null=True, blank=True)
+    avaliacoes = models.ManyToManyField(Avaliacao, null=True, blank=True)
+    endereco = models.ForeignKey(Endereco, on_delete=models.CASCADE, null=True, blank=True)
 
     def __str__(self):
         return self.nome
