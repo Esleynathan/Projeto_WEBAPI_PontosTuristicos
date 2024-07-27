5fbdb720d8f7f9b9680f4ffc7e014541f7cbde37
Esley <esleynathan@hotmail.com>
2024-07-27 00:00:19 -0300

    Nested Relationships - Foreignkeys

--- core/api/serializers.py
+++ core/api/serializers.py
@@ -4,10 +4,11 @@ from atracoes.api.serializers import AtracaoSerializer
 from enderecos.api.serializers import EnderecoSerializer
 from rest_framework.fields import SerializerMethodField
 from atracoes.models import Atracao
+from enderecos.models import Endereco
 
 class PontoTuristicoSerializer(ModelSerializer):
     atracoes = AtracaoSerializer(many=True)
-    endereco = EnderecoSerializer(read_only=True)
+    endereco = EnderecoSerializer()
     descricao_completa = SerializerMethodField()
 
     class Meta:
@@ -16,7 +17,7 @@ class PontoTuristicoSerializer(ModelSerializer):
                   'atracoes', 'comentarios', 'avaliacoes', 'endereco',
                   'descricao_completa', 'descricao_completa2'
                   ]
-        read_only_fields = ('nome', 'descricao', 'comentarios', 'avaliacoes')
+        read_only_fields = ('comentarios', 'avaliacoes')
     
     def cria_atracoes(self, atracoes, ponto):
         for atracao in atracoes:
@@ -24,13 +25,21 @@ class PontoTuristicoSerializer(ModelSerializer):
             ponto.atracoes.add(at)
 
 
-    def create (self, validated_data):
-        print(validated_data['atracoes'])        
+    def create (self, validated_data):     
         atracoes = validated_data['atracoes']
         del validated_data['atracoes']
+
+        endereco = validated_data['endereco']
+        del validated_data['endereco']
+
         ponto = PontoTuristico.objects.create(**validated_data)
         self.cria_atracoes(atracoes, ponto)
 
+        end = Endereco.objects.create(**endereco)
+        ponto.endereco = end
+
+        ponto.save()
+        
         return ponto
 
 
