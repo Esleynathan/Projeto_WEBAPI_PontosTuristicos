fda253c7ab300cf620894d9059daeeb2d54f7d6f
Esley <esleynathan@hotmail.com>
2024-08-14 16:10:51 -0300

    Criando requeriments e finalizando projeto

--- core/models.py
+++ core/models.py
@@ -11,9 +11,9 @@ class PontoTuristico(models.Model):
     nome = models.CharField(max_length=150)
     descricao = models.TextField()
     aprovado = models.BooleanField(default=False)
-    atracoes = models.ManyToManyField(Atracao, null=True, blank=True)
-    comentarios = models.ManyToManyField(Comentario, null=True, blank=True)
-    avaliacoes = models.ManyToManyField(Avaliacao, null=True, blank=True)
+    atracoes = models.ManyToManyField(Atracao, blank=True)
+    comentarios = models.ManyToManyField(Comentario, blank=True)
+    avaliacoes = models.ManyToManyField(Avaliacao, blank=True)
     endereco = models.ForeignKey(Endereco, on_delete=models.CASCADE, null=True, blank=True)
 
     foto = models.ImageField(upload_to='pontos_turisticos', null=True, blank=True)
